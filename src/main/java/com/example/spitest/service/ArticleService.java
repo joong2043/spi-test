185b9cd2a3d08466c0dae68c1634aa779780ea02
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-07-27 23:38:41 +0900

    Application 레벨 락 구현 및 멀티스레드 환경 테스트

--- src/main/java/com/example/spitest/service/ArticleService.java
+++ src/main/java/com/example/spitest/service/ArticleService.java
@@ -3,8 +3,11 @@ package com.example.spitest.service;
 import com.example.spitest.domain.Article;
 import com.example.spitest.repository.ArticleRepository;
 import java.time.LocalDateTime;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,8 +15,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
-    private final Lock lock = new ReentrantLock();
+//    private final Lock lock = new ReentrantLock();
 
+    private final ConcurrentHashMap<Long, Lock> articleLocks = new ConcurrentHashMap<>();
+
+    private final Logger logger = LoggerFactory.getLogger(ArticleService.class);
 
     public ArticleService(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
@@ -29,25 +35,29 @@ public class ArticleService {
 
     @Transactional
     public Article edit(Long articleId, Long editorId, String editContent) throws Exception {
+        // 해당 게시글에 락을 확보하기
+        Lock lock = articleLocks.computeIfAbsent(articleId, id -> new ReentrantLock());
+
         if (lock.tryLock()) {
             try {
+
                 Article article = articleRepository.findArticleById(articleId).orElseThrow(() -> new Exception("Not Found Id"));
 
-                System.out.println("락이 걸립니다 에디터:" + editorId + " | 락이 걸린 시간:" + LocalDateTime.now());
+                logger.info("락이 걸립니다 에디터:" + editorId + " | 락이 걸린 시간:" + LocalDateTime.now());
 
-                Thread.sleep(5000);
+                Thread.sleep(3000);
 
                 article.updateContent(editContent);
 
                 return article;
             } finally {
                 lock.unlock();
-                System.out.println("락이 해제됐습니다 에디터:" + editorId + " | 락이 해제된 시간:" + LocalDateTime.now());
+                logger.info("락이 해제됐습니다 에디터:" + editorId + " | 락이 해제된 시간:" + LocalDateTime.now());
 
             }
         }
         else {
-            System.out.println("이미 다른 에디터가 글을 수정중입니다. 수정이 완료될 때 까지 기다려주세요");
+            logger.info("(충돌) 이미 다른 에디터가 글을 수정중입니다. 수정이 완료될 때 까지 기다려주세요. 충돌 시간 : " + LocalDateTime.now() +", 수정 내용 : "+editContent);
             return null;
         }
     }
--- src/test/java/com/example/spitest/Article/ArticleServiceTest.java
+++ src/test/java/com/example/spitest/Article/ArticleServiceTest.java
@@ -7,21 +7,39 @@ import com.example.spitest.service.ArticleService;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ArticleServiceTest {
-
     @Autowired
     private ArticleService articleService;
 
     @Autowired
     private EditorRepository editorRepository;
 
+    @BeforeAll
+    public void 데이터_넣기() {
+
+        for (int i = 1; i <= 10; i++) {
+            editorRepository.save(new Editor());
+        }
+
+        articleService.save("첫번째 게시글", "content0");
+        articleService.save("두번째 게시글", "content0");
+    }
+
     @Test
-    public void 여러명이_동시에_글을_수정() throws Exception {
+    public void 락이_잘_작동하는지_테스트() throws Exception {
+
         int threadCount = 10;
 
         ExecutorService executorService = Executors.newFixedThreadPool(10);
@@ -29,20 +47,15 @@ public class ArticleServiceTest {
         // 다른 스레드에서 수행하는 작업을 기다리도록 도와줌
         CountDownLatch latch = new CountDownLatch(threadCount);
 
-        for (int i = 1; i <= 10; i++) {
-            editorRepository.save(new Editor());
-        }
+        AtomicInteger collisionCount = new AtomicInteger(0);
 
-        articleService.save("첫번째 게시글","content0");
-        articleService.save("두번째 게시글","content0");
+        executorService.submit(() -> {
 
-        for (int i = 1; i <= 5; i++) {
+            for (int i = 1; i <= 10; i++) {
+                long editorId = i;
 
-            long editorId = i;
-
-            executorService.submit(() -> {
                 try {
-                    Article article = articleService.edit(1L, editorId, "content edit "+editorId);
+                    Article article = articleService.edit(2L, editorId, "content edit " + editorId);
 
                     System.out.println("글 수정 성공 : " + article.getContent());
                 } catch (Exception e) {
@@ -50,21 +63,38 @@ public class ArticleServiceTest {
                 } finally {
                     latch.countDown();
                 }
-            });
 
-            Thread.sleep(5000);
-        }
+            }
+        });
 
-        for (int i = 6; i <= 10; i++) {
+        latch.await();
+
+        assert (collisionCount.get() == 0);
+    }
+
+    @Test
+    public void 여러명이_동시에_글을_수정할_때_충돌_테스트() throws Exception {
+
+        int threadCount = 10;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        // 다른 스레드에서 수행하는 작업을 기다리도록 도와줌
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        AtomicInteger collisionCount = new AtomicInteger(0);
+
+        for (int i = 1; i <= 10; i++) {
 
             long editorId = i;
 
             executorService.submit(() -> {
-                try {
-                    Article article = articleService.edit(2L, editorId, "content edit "+editorId);
 
+                try {
+                    Article article = articleService.edit(1L, editorId, "content edit " + editorId);
                     System.out.println("글 수정 성공 : " + article.getContent());
                 } catch (Exception e) {
+                    collisionCount.incrementAndGet();
                     throw new RuntimeException(e);
                 } finally {
                     latch.countDown();
@@ -73,5 +103,65 @@ public class ArticleServiceTest {
         }
 
         latch.await();
+
+        assert(collisionCount.get() == 9);
+
+    }
+
+    @Test
+    public void 첫번째_글과_2번째_글을_동시에_수정_가능한지_테스트() throws InterruptedException {
+        int threadCount = 10;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        // 다른 스레드에서 수행하는 작업을 기다리도록 도와줌
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        AtomicInteger collisionCount1 = new AtomicInteger(0);
+        AtomicInteger collisionCount2 = new AtomicInteger(0);
+
+
+        executorService.submit(() -> {
+
+            for (int i = 1; i <= 5; i++) {
+
+                long editorId = i;
+
+                try {
+                    Article article = articleService.edit(1L, editorId, "content edit " + editorId);
+
+                    System.out.println("글 수정 성공 : " + article.getContent());
+                } catch (Exception e) {
+                    collisionCount1.getAndIncrement();
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            }
+
+        });
+
+        executorService.submit(() -> {
+
+            for (int i = 6; i <= 10; i++) {
+
+                long editorId = i;
+
+                try {
+                    Article article = articleService.edit(2L, editorId, "content edit " + editorId);
+
+                    System.out.println("글 수정 성공 : " + article.getContent());
+                } catch (Exception e) {
+                    collisionCount2.getAndIncrement();
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
+
+        latch.await();
+
+        assert (collisionCount1.get()==0 && collisionCount2.get()==0);
     }
 }
